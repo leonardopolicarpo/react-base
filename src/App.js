09fc0c85d610de6c1293e2e049b60c105a8621f9
leonardo-policarpo <leo.dumont@outlook.com>
2023-09-28 11:36:19 -0300

    router e history

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 
+import history from './services/history';
 import GlobalStyles from './styles/GlobalStyles';
-import Login from './pages/Login';
 import Header from './components/Header';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <>
+    <Router history={history}>
       <Header />
-      <Login />
+      <Routes />
       <GlobalStyles />
-    </>
+    </Router>
   );
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
 import { Nav } from './styles';
 
 export default function Header() {
   return (
     <Nav>
-      <a href="http://youtube.com">
+      <Link to="/">
         <FaHome size={24} />
-      </a>
-      <a href="http://google.com">
+      </Link>
+      <Link to="/login">
         <FaUserAlt size={24} />
-      </a>
-      <a href="http://google.com">
+      </Link>
+      <Link to="/asdasd">
         <FaSignInAlt size={24} />
-      </a>
+      </Link>
     </Nav>
   );
 }
